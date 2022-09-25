7e109d2a539b0492345413b2df6562182725468e
Jackalob <nike9712@gmail.com>
2022-09-25 17:54:02 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  devServer: {
+    port: "3000",
+    static: path.resolve(__dirname, "public"),
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
