fafe9b9f08ab42a82063851814e54f4c47a4bf57
Jackalob <nike9712@gmail.com>
2022-09-27 00:42:28 +0800

    add tailwind settings

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+import "./assets/css/app.css";
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 
